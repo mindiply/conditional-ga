ae066d207c0f56b568346646cb876ea10bd7492a
Paolo Ferdinando Bongiovanni <paolo.bongiovanni@mindiply.com>
2017-05-30 16:58:23 +0100

    Initial test running fine

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@
  */
 /* global ga USE_GA */
 let useGa = false
+function _gaCheck () {
+    return useGa && ga
+}
 
 export function initGA (propertyId, options = {}) {
-    if (!propertyId) return
     if (options && options.checkFn && typeof checkFn === 'function') {
         useGa = options.checkFn()
         if (!useGa) return
@@ -14,17 +16,22 @@ export function initGA (propertyId, options = {}) {
         useGa = useGa || window.USE_GA === true
     }
     if (options && !options.DISABLE_GLOBAL_SCOPE_CHECK) {
-        useGa = useGa || USE_GA === true
+        if (typeof USE_GA !== 'undefined') {
+            useGa = useGa || USE_GA === true
+        }
     }
     if (options && options.locationRegEx && options.locationRegEx instanceof RegExp) {
         if (!options.locationRegEx.test(window.location.host)) {
             useGa = false
             return
         }
+        useGa = true
     }
     if (!useGa) return
     /* eslint-disable */
     if (typeof ga === 'undefined') {
+        // Initialize only if it's not done already via template scripts
+        if (!propertyId) return
         (function (i, s, o, g, r, a, m) {
             i['GoogleAnalyticsObject'] = r
             i[r] = i[r] || function () {
@@ -36,14 +43,19 @@ export function initGA (propertyId, options = {}) {
             a.src = g
             m.parentNode.insertBefore(a, m)
         })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
+        /* eslint-enable */
+        ga(() => {
+            const trackers = ga.getAll()
+            if (!(trackers && trackers.length > 0)) {
+                ga('create', propertyId, 'auto', options && options.gaCreateOptions ? options.gaCreateOptions : {})
+            }
+        })
     }
-    /* eslint-enable */
-    ga('create', propertyId, 'auto', options && options.gaCreateOptions ? options.gaCreateOptions : {})
 }
 
 export function recordScreenRender (screenName) {
     try {
-        if (useGa) {
+        if (_gaCheck()) {
             ga('send', 'screenview', {
                 screenName
             })
@@ -55,7 +67,7 @@ export function recordScreenRender (screenName) {
 
 export function recordEvent (category, action, label, value) {
     try {
-        if (useGa) {
+        if (_gaCheck()) {
             ga('send', 'event', category, action, label, value)
         }
     } catch (err) {
@@ -64,12 +76,14 @@ export function recordEvent (category, action, label, value) {
 }
 
 let oldView = null
-export function recordNewView (view = window.url.pathname) {
+export function recordNewView (view) {
     try {
-        if (useGa) {
+        if (_gaCheck()) {
             if (oldView !== view) {
                 oldView = view
-                ga('set', 'page', view)
+                if (view) {
+                    ga('set', 'page', view)
+                }
                 ga('send', 'pageview')
             }
         }
